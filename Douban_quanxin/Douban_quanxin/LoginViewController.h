38e1a79415b9be7468c74a4e8dfb76e6471e4119
lkjy <yang@lkjydemac-mini-5.local>
2016-05-05 20:09:47 +0800

    ff

---
+++ Douban_quanxin/Douban_quanxin/LoginViewController.h
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.h
+//  Douban
+//
+//  Created by YANG on 16/5/5.
+//  Copyright © 2016年 唐家文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *captchaImageView;
+@property (weak, nonatomic) IBOutlet UITextField *username;
+@property (weak, nonatomic) IBOutlet UITextField *password;
+@property (weak, nonatomic) IBOutlet UITextField *captcha;
+- (IBAction)submitButtonTapped:(UIButton *)sender;
+- (IBAction)cancelButtonTapped:(UIButton *)sender;
+- (IBAction)backgroundTap:(id)sender;
+
+@end
