eb77417b540ce4f2e225e32e780e324355b0844c
LIZ <1104074113@qq.com>
2016-05-05 19:59:26 +0800

    李one

---
+++ Douban_quanxin/Douban_quanxin/ChannelsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChannelsTableViewController.h
+//  Douban_quanxin
+//
+//  Created by lkjy on 16/5/5.
+//  Copyright © 2016年 唐家文. All rights reserved.
+//li
+
+#import <UIKit/UIKit.h>
+
+@interface ChannelsTableViewController : UITableViewController
+
+@end
